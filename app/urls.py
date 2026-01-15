88367118e3ac65b792784c6ae99c4ecf3a6596af
MirnaPenaforte <mirnapenaforte.dev@gmail.com>
2026-01-14 21:19:54 -0300

    refatorando funções view

--- app/urls.py
+++ app/urls.py
@@ -18,14 +18,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import cars_views
-from cars.views import new_car_view
+from cars.views import CarsView
+from cars.views import NewCarView
 from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cars/', cars_views, name='cars_list'),
-    path('new_car/', new_car_view, name='new_car' ),
+    path('cars/', CarsView.as_view(), name='cars_list'),
+    path('new_car/', NewCarView.as_view(), name='new_car' ),
     path('register/', register_view, name='register'),
     path('login/', login_view, name= 'login'),
     path('logout/', logout_view, name='logout')
--- cars/views.py
+++ cars/views.py
@@ -1,28 +1,50 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 
-# Create your views here.
-def cars_views(request):
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
 
-    if search:
-        cars = Car.objects.filter(model__icontains = search).order_by('model')
+class CarsView(View):
+#classe Based View, view que herda propriedades de View.
 
-    return render(
-        request, 
-        'cars.html',
-        {'cars': cars}
+    #função executada após a propriedade dispatch() ser executada e reconhecer um metodo get
+    def get(self, request):
+        #a variavel cars são todos os objetos cars do banco de dados ordenados por Model
+        cars = Car.objects.all().order_by('model')
+        #serch usa o metodo get para pegar o input
+        search = request.GET.get('search')
+        #se o search for usado, ele faz uma busca no banco de dados pelo input
+        if search:
+            cars = Car.objects.filter(model__icontains = search).order_by('model')
+        #função que renderiza a view 
+        return render(
+            request, 
+            #pagina html onde essa view sera renderizada
+            'cars.html',
+            {'cars': cars}
         
         )
+#method fuction based views
+# def new_car_view(request):
+#     if request.method == 'POST':
+#         new_car_form = CarModelForm(request.POST, request.FILES)
+#         if new_car_form.is_valid():
+#             new_car_form.save()
+#             return redirect('cars_list')
+#     else:
+#         new_car_form = CarModelForm()
+#     return render(request, 'new_car.html', {'new_car_form':new_car_form})
 
-def new_car_view(request):
-    if request.method == 'POST':
+class NewCarView(View):
+    
+    def get(self, request):
+        new_car_form = CarModelForm()
+        return render(request, "new_car.html", {'new_car_form': new_car_form})
+    
+    def post(self, request):
         new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
-    else:
-        new_car_form = CarModelForm()
-    return render(request, 'new_car.html', {'new_car_form':new_car_form})
+        return render(request, "new_car.html", {'new_car_form': new_car_form})
+        
